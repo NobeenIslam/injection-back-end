ef6041cca9ecfd4dca0529882f9653515311bf6b
Nobeen <n.islam.1@outlook.com>
2022-04-23 17:04:04 +0100

    initial database queries

---
+++ src/createTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE userinfo (
+    id INTEGER PRIMARY KEY,
+    username TEXT,
+    password TEXT
+);
+
+INSERT INTO userinfo (username, password)
+VALUES 
+    ('MrPooey', 'vwh64ccn'),
+    ('ATCashew', 'superman');
