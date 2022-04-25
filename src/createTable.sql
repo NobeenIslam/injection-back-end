23b7eb290c2aedfe40693bcf966085ee637ce342
Nobeen <n.islam.1@outlook.com>
2022-04-25 02:11:41 +0100

    changed id type of tables to serial and added inventory table

--- src/createTable.sql
+++ src/createTable.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS userinfo;
+
 CREATE TABLE userinfo (
-    id INTEGER PRIMARY KEY,
+    id serial PRIMARY KEY,
     username TEXT,
     password TEXT
 );
@@ -8,3 +10,16 @@ INSERT INTO userinfo (username, password)
 VALUES 
     ('MrPooey', 'vwh64ccn'),
     ('ATCashew', 'superman');
+
+DROP TABLE IF EXISTS inventory;
+
+CREATE TABLE inventory (
+    id SERIAL PRIMARY KEY,
+    itemname TEXT,
+    count INTEGER
+);
+
+INSERT INTO inventory (itemname,count)
+VALUES 
+    ('hammer',3),
+    ('spoon',3);
